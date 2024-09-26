fb3668299ec86832e7d3ae6e9f948c81cb11b6a1
Arthur Batista <52763653+artbatistat@users.noreply.github.com>
2024-09-26 18:24:57 -0300

    Add files via upload
    This new file there are new features, as error handling.

--- ConsultaCEP-API.py
+++ ConsultaCEP-API.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def consultar_cep(cep):
   url = f'https://viacep.com.br/ws/{cep}/json/'
@@ -6,6 +7,7 @@ def consultar_cep(cep):
   r = requests.get(url)
   if (r.status_code == 200):
       print(
+            f'\n'
             f'Cep: {r.json()["cep"]} \n'
             f'Rua: {r.json()["logradouro"]} \n'
             f'Bairro: {r.json()["bairro"]} \n'   
@@ -14,7 +16,10 @@ def consultar_cep(cep):
             )
   else:
       if(r.status_code >= 400) and (r.status_code <= 499):
-        print(f'{r.status_code} - Error related with client error responses')
+        print(
+              f'\n'
+              f'{r.status_code} - Error related with client error responses'
+              )
       elif(r.status_code >= 500) and (r.status_code <= 599):
         print(f'{r.status_code} - Error related with server error responses')
       else:
@@ -31,7 +36,16 @@ while(condition == False):
   input_cep = input("Digite o CEP:")
   if(len(input_cep) == 8):
     condition = True
+  elif (len(input_cep) == 9) and (re.search(r'[-]',input_cep)):
+    cep_split = input_cep.split("-")
+    input_cep = cep_split[0]+cep_split[1]
+    condition = True
   else:
+    print(
+          f'Alguma coisa está errada... \n'
+          f'Tente digitar somente números. \n'
+          f'Por exemplo: 32671674'
+          )
     condition = False
 
 consultar_cep(input_cep)
